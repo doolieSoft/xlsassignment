93d0f7dedf20dbcf56c3c55dae3841de2423cb62
stef78ano <s.crapanzano@gmail.com>
2017-02-17 16:48:42 +0100

    Some refactoring et ajout du ratio dans le listbox des agents

--- assignmentFormCode.vb
+++ assignmentFormCode.vb
@@ -1,34 +1,73 @@
+Private Sub AssignButton_Click()
+    Debug.Print ListAgent.Value
+    For r = 0 To ListAgent.ListCount - 1
+    If ListAgent.Selected(r) Then
+        Sheets("Total").Cells(10, 10) = "coucou"
+        Debug.Print "You selected row #" & r + 1
+        Debug.Print ListAgent.List(r, 1)
+    End If
+Next
+End Sub
+
 Private Sub CI_Change()
     Dim ciName As String
     Dim ciRange As Range
     Dim ciLine As Integer
-    
+    Dim nbAgent As Integer
+    Dim agentName As String
+    Dim agentColumn As Integer
+
     ciName = CI.Value
     Set ciRange = Worksheets("Matrix").Range("A1:A10000").Find(ciName, lookat:=xlPart)
     ciLine = ciRange.Row
-    Debug.Print (ciLine)
-    
     ListAgent.Clear
     
-    Dim nbAgent As Integer
-    Dim agentName As String
-    
     nbAgent = getLastAgentColumn
-    For i = 1 To nbAgent
-        If Sheets("Matrix").Cells(ciLine, i) > 0 Then
-            addAgentToListAgent (i)
-        End If
-    Next i
+    For agentColumn = 2 To nbAgent
+        agentName = Sheets("Matrix").Cells(1, agentColumn)
+        If IsNumeric(Sheets("Matrix").Cells(ciLine, agentColumn)) Then
+                
+            If Sheets("Matrix").Cells(ciLine, agentColumn) > 0 Then
+                Dim idLastAgentAdded As Integer
+                idLastAgentAdded = addAgentToListAgent(agentName)
+                
+                Dim totalTicketsForCISelected As Integer
+                Dim ratio As Double
+                Dim grandTotalForAgent As Integer
+                
+                totalTicketsForCISelected = Sheets("Total").Cells(ciLine, agentColumn)
+                grandTotalForAgent = getGrandTotalForAgent(agentColumn)
+                ratio = 0
     
-    ListAgent.AddItem
+                If grandTotalForAgent > 0 Then
+                    ratio = Round(totalTicketsForCISelected / grandTotalForAgent * 100, 2)
+                End If
+                updateRatioInListAgent idLastAgentAdded, ratio
+                updateTotalInListAgent idLastAgentAdded, totalTicketsForCISelected
+            End If
+        End If
+    Next agentColumn
 End Sub
 
 Private Function getLastAgentColumn() As Integer
     getLastAgentColumn = Sheets("Matrix").Cells(1, Columns.Count).End(xlToLeft).column
 End Function
 
-Private Sub addAgentToListAgent(agentColumn As Integer)
-    If Cells(1, agentColumn) <> "" Then
-        ListAgent.AddItem (Cells(1, agentColumn))
+Private Function addAgentToListAgent(agentName As String) As Integer
+    If agentName <> "" Then
+        ListAgent.AddItem (agentName)
     End If
+    addAgentToListAgent = ListAgent.ListCount
+End Function
+
+Private Sub updateTotalInListAgent(id As Integer, totalTicket As Integer)
+    ListAgent.List(ListAgent.ListCount - 1, 2) = totalTicket
 End Sub
+
+Private Sub updateRatioInListAgent(idLastAgentAdded As Integer, ratio As Double)
+    ListAgent.List(ListAgent.ListCount - 1, 1) = ratio
+End Sub
+
+Private Function getGrandTotalForAgent(agentColumn As Integer) As Integer
+    getGrandTotalForAgent = Application.WorksheetFunction.Sum(Range(Sheets("Total").Cells(2, agentColumn), Sheets("Total").Cells(ThisWorkbook.getLastCIRow, agentColumn)))
+End Function
--- thisWorkbook.vb
+++ thisWorkbook.vb
@@ -2,11 +2,12 @@ Option Explicit
 
 Dim ciCollection As New Collection
 Dim agentCollection As New Collection
-
 Dim MatrixSheetName As String
+Dim TotalSheetName As String
 
 Public Sub ShowAssignmentForm()
     MatrixSheetName = "Matrix"
+    TotalSheetName = "Total"
     
     initializeCICollection
     initializeAgentCollection
@@ -20,6 +21,7 @@ End Sub
 
 Private Sub initializeCICollection()
     Dim currentCellLine As Integer
+    
     For currentCellLine = 2 To getLastCIRow()
         Dim ciName As String
         ciName = getCiNameAtLine(currentCellLine)
@@ -28,21 +30,34 @@ Private Sub initializeCICollection()
 End Sub
 
 Private Sub initializeAgentCollection()
+    Dim currentCellColumn As Integer
+    
+    For currentCellColumn = 2 To getLastAgentColumn()
+        Dim agentName As String
+        agentName = getAgentNameAtColumn(currentCellColumn)
+        agentCollection.Add (agentName)
+    Next currentCellColumn
+    
 End Sub
 
 Private Sub initializeCIBox()
     Dim Item As Variant
+    
     For Each Item In ciCollection
         AssignmentForm.CI.AddItem (Item)
     Next Item
 End Sub
 
+Public Function getLastCIRow() As Integer
+    getLastCIRow = Sheets(MatrixSheetName).Range("A" & Rows.Count).End(xlUp).Row
+End Function
+
 Private Function getCiNameAtLine(line As Integer) As String
     getCiNameAtLine = Sheets(MatrixSheetName).Cells(line, 1)
 End Function
 
-Private Function getLastCIRow() As Integer
-    getLastCIRow = Sheets(MatrixSheetName).Range("A" & Rows.Count).End(xlUp).Row
+Private Function getAgentNameAtColumn(column As Integer) As String
+    getAgentNameAtColumn = Sheets(MatrixSheetName).Cells(1, column)
 End Function
 
 Private Function getLastAgentColumn() As Integer
